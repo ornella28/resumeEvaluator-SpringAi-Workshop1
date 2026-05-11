dc3c31665574cc6ca7ab217c0de7a8e206b90f9c
ornella28 <angelnibaruta88@gmail.com>
2026-05-11 14:27:39 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/resumeevaluatorspringaiworkshop1/ResumeEvaluatorSpringAiWorkshop1Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.resumeevaluatorspringaiworkshop1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeEvaluatorSpringAiWorkshop1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeEvaluatorSpringAiWorkshop1Application.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/resumeevaluatorspringaiworkshop1/ResumeEvaluatorSpringAiWorkshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.resumeevaluatorspringaiworkshop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeEvaluatorSpringAiWorkshop1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
